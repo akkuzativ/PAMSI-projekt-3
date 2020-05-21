a5ad5d855f765bd34c8083195d67a729ba43592f
akkuzativ <goreckialeksander9@gmail.com>
2020-05-21 23:28:09 +0200

    .

---
+++ src/board.cpp
@@ -0,0 +1,105 @@
+#include "../inc/board.hh"
+
+
+std::pair<Field::Type, Field::Type> Field::getEnemy()
+{
+    if (this->type == RED || this->type == RED)
+    {
+        return {WHITE, WHITEKING};
+    }
+    else
+    {
+        return {RED, REDKING};
+    } 
+}
+
+
+
+std::pair<Field::Type, Field::Type> Field::getEnemy(Type type)
+{
+    if (type == RED || type == RED)
+    {
+        return {WHITE, WHITEKING};
+    }
+    else
+    {
+        return {RED, REDKING};
+    } 
+}
+
+
+
+Board::Board()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if ((i+j)%2 == 0)
+            {
+                _board[i][j].type = Field::INVALID;
+            }
+            else
+            {
+                if (i < 3)
+                {
+                    _board[i][j].type = Field::RED;
+                }
+                else if (i > 4)
+                {
+                    _board[i][j].type = Field::WHITE;
+                }
+                else
+                {
+                    _board[i][j].type = Field::FREE;
+                }
+            }
+            _board[i][j].positionOnBoard = {i, j};
+        }
+    }
+}
+
+
+
+bool Board::checkRegularMovePotential(Position position)
+{
+    if (position.row < 0 || position.row > 7 || position.column < 0 || position.column >> 7)
+    {
+        return false;
+    }
+    if (_board[position.row][position.column].type != Field::FREE)
+    {
+        return false;
+    }
+
+
+    return true;
+}
+
+
+
+bool Board::checkJumpPotential(Position pieceToBeJumped, Position landingPosition, Field::Type currentTurn)
+{
+    if (pieceToBeJumped.row < 0 || pieceToBeJumped.row > 7 || pieceToBeJumped.column < 0 || pieceToBeJumped.column >> 7)
+    {
+        return false;
+    }
+
+    if (landingPosition.row < 0 || landingPosition.row > 7 || landingPosition.column < 0 || landingPosition.column >> 7)
+    {
+        return false;
+    }
+
+    if (_board[pieceToBeJumped.row][pieceToBeJumped.column].type != Field::getEnemy(currentTurn).first || _board[pieceToBeJumped.row][pieceToBeJumped.column].type != Field::getEnemy(currentTurn).second)
+    {
+        return false;
+    }
+    
+    if (_board[landingPosition.row][landingPosition.column].type != Field::FREE)
+    {
+        return false;
+    }
+
+    
+    return true;
+}
