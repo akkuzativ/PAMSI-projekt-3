2fa414daf89b9b208648d11b6c1e5f3c70d084c9
Aleksy <goreckialeksander9@gmail.com>
2020-05-23 03:20:35 +0200

    .

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -51,21 +51,27 @@ bool Renderer::drawBoard(Board gameboard, sf::RenderWindow& window)
             switch (gameboard(i, j).type)
             {
             case Field::INVALID:
+                fieldSprites[0].setPosition();
                 window.draw(fieldSprites[0]);
                 break;
             case Field::FREE:
+                fieldSprites[0].setPosition();
                 window.draw(fieldSprites[1]);
                 break;
             case Field::RED:
+                fieldSprites[0].setPosition();
                 window.draw(fieldSprites[2]);
                 break;
             case Field::WHITE:
+              fieldSprites[0].setPosition();
                 window.draw(fieldSprites[3]);
                 break;    
             case Field::REDKING:
+                fieldSprites[0].setPosition();
                 window.draw(fieldSprites[4]);
                 break;
             case Field::WHITEKING:
+                fieldSprites[0].setPosition();
                 window.draw(fieldSprites[5]);
                 break;
             }
